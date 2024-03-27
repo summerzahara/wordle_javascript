6bc244122dee1c96225d0024eb17385b0c21ba61
Summer Zahara <92226335+summerzahara@users.noreply.github.com>
2024-03-27 14:24:20 -0700

    Initial Commit
    HTML Setup

---
+++ main.js
@@ -0,0 +1,20 @@
+// Rows in board
+const firstWord = document.querySelector('form').firstElementChild
+const secondWord = document.querySelector('form').children[1]
+const thirdWord = document.querySelector('form').children[2]
+const fourthWord = document.querySelector('form').children[3]
+const fifthWord = document.querySelector('form').children[4]
+const sixthWord = document.querySelector('form').lastElementChild
+const wordRows = [firstWord, secondWord, thirdWord, fourthWord, fifthWord, sixthWord]
+
+
+
+// row.children.forEach(item => console.log(item))
+
+// row.children.forEach(item => console.log(item))
+
+console.log(thirdWord);
+
+// Event listeners
+
+
