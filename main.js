75295d235e2809d5902f8a4ee713490ab1279230
Summer Zahara <92226335+summerzahara@users.noreply.github.com>
2024-03-28 13:59:54 -0700

    Keyboard Event Listeners

--- main.js
+++ main.js
@@ -1,20 +1,65 @@
-// Rows in board
-const firstWord = document.querySelector('form').firstElementChild
-const secondWord = document.querySelector('form').children[1]
-const thirdWord = document.querySelector('form').children[2]
-const fourthWord = document.querySelector('form').children[3]
-const fifthWord = document.querySelector('form').children[4]
-const sixthWord = document.querySelector('form').lastElementChild
-const wordRows = [firstWord, secondWord, thirdWord, fourthWord, fifthWord, sixthWord]
+// Keyboard
+const keys = document.querySelectorAll('#key')
+const submit = document.querySelector('#key-enter')
 
+// Game Board
+const firstWord = document.querySelector('#word-1')
+console.log(firstWord.childNodes[0])
 
+// Game Functionality
+let currWord = []
+const wordsGuessed = []
 
-// row.children.forEach(item => console.log(item))
+// console.log(keys);
 
-// row.children.forEach(item => console.log(item))
+function enterLetter(e) {   
+    const letter = e.target.innerText
+    // console.log(letter)
+    if (currWord.length < 5) {
+        currWord.push(letter)
+        letterOnScreen(currWord)
+    } else {
+        console.log('Word only 5 letters.')
+    }
+    console.log(currWord)
+}
 
-console.log(thirdWord);
+function letterOnScreen(word) {
+    console.log(word.length)
+    firstWord.childNodes[word.length].value = word[word.length-1]
+}
+
+function guessWord(e) {
+    if (checkGame()) {
+        if (currWord.length = 5) {
+            wordsGuessed.push(currWord.join(''))
+            resetCurrWord()
+            console.log(wordsGuessed)
+        }
+    } else {
+        console.log('Game Over')
+    }
+    }
+
+function resetCurrWord() {
+    currWord = []
+}
+
+function checkGame() {
+    if (wordsGuessed.length < 6) {
+        return true
+    } else {
+        return false
+    }
+}
 
 // Event listeners
+keys.forEach(key => {
+    key.addEventListener('click', enterLetter);
+})
+
+submit.addEventListener('click', guessWord)
+
+
 
 
